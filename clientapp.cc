cbc48b5c54104dc35b220c6212a3d3a4b4200293
scenic <scenic.wow@gmail.com>
2016-09-05 09:21:43 +0800

    add js object
    reference:
    
    https://bitbucket.org/chromiumembedded/cef/wiki/JavaScriptIntegration.md#markdown-header-js-functions

--- clientapp.cc
+++ clientapp.cc
@@ -74,10 +74,18 @@ void CCefClientApp::OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<Ce
 	CefRefPtr<CefV8Value> object = context->GetGlobal();// ªÒ»°µΩwindow  
 	CefRefPtr<CefV8Value> str = CefV8Value::CreateString("C++ created Value!");
 	object->SetValue("jsValue", str, V8_PROPERTY_ATTRIBUTE_NONE);
+
+	CefRefPtr<CefV8Accessor> accessor=new MyV8Accessor;
+	CefRefPtr<CefV8Value> obj = CefV8Value::CreateObject(accessor);
+
+	obj->SetValue("myval", V8_ACCESS_CONTROL_DEFAULT,V8_PROPERTY_ATTRIBUTE_NONE);
+
+	object->SetValue("myobject", obj, V8_PROPERTY_ATTRIBUTE_NONE);
+
 }
 
 
 void CCefClientApp::OnContextReleased(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context)
 {
-
+	v8Handler_ = nullptr;
 }
--- clientapp.h
+++ clientapp.h
@@ -38,3 +38,46 @@ private:
 private:
 
 };
+
+
+class MyV8Accessor : public CefV8Accessor {
+public:
+	MyV8Accessor() {}
+
+	virtual bool Get(const CefString& name,
+		const CefRefPtr<CefV8Value> object,
+		CefRefPtr<CefV8Value>& retval,
+		CefString& exception) OVERRIDE {
+		if (name == "myval") {
+			// Return the value.
+			retval = CefV8Value::CreateString(myval_);
+			return true;
+		}
+
+		// Value does not exist.
+		return false;
+	}
+
+	virtual bool Set(const CefString& name,const CefRefPtr<CefV8Value> object,const CefRefPtr<CefV8Value> value,CefString& exception) override {
+		if (name == "myval") {
+			if (value->IsString()) {
+				// Store the value.
+				myval_ = value->GetStringValue();
+			}
+			else {
+				// Throw an exception.
+				exception = "Invalid value type";
+			}
+			return true;
+		}
+
+		// Value does not exist.
+		return false;
+	}
+
+	// Variable used for storing the value.
+	CefString myval_;
+
+	// Provide the reference counting implementation for this class.
+	IMPLEMENT_REFCOUNTING(MyV8Accessor);
+};
